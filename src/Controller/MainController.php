caa696c3594d660e13a6640f879457a7a2ce9cd8
vdediego <vdediego@atsistemas.com>
2020-02-04 14:43:19 +0100

    require doctrine

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +12,16 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(): Response
     {
-        return new Response('');
+        return $this->render('post/post-listing.html.twig');
+    }
+
+    /**
+     * @Route("/custom", name="custom")
+     */
+    public function custom(): Response
+    {
+        return $this->render('post-listing.html.twig');
     }
 }
