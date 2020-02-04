f3da4a8230f50eb149096e82916698ee35bee3e4
vdediego <vdediego@atsistemas.com>
2020-02-04 14:42:28 +0100

    enable SASS and TS

--- webpack.config.js
+++ webpack.config.js
@@ -54,10 +54,10 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+    .enableTypeScriptLoader()
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
